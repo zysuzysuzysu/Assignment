636aea0d6ab15472d90ea9951e793215d2f646de
Suzanne Manongas <zysuzysuzysu@gmail.com>
2020-08-01 22:04:11 -0500

    Creating classes with their functionality

--- Assignment_9.1_Manongas.py.py
+++ Assignment_9.1_Manongas.py.py
@@ -1,7 +1,7 @@
 ''' 
 Suzanne Marie Mannongas
 Assignment 8.1
-July 30, 2020
+August 1, 2020
 
 Creating a program that performs file processing activities:
 	- using OS library to validate directory exist
@@ -11,41 +11,50 @@ Creating a program that performs file processing activities:
 	- read file written and display contents
 '''
 
-import os.path
-import json
+import os # importing OS library
 
-print(os.getcwd())
-print(os.listdir())
+class Files:
+	def __init__(self, directory, filename):
+		self.directory = directory
+		self.filename = filename
 
-def AskInformation():
-	name = input("Name: ").split(",")
-	address = input("Address: ").split(",")
-	phone_num = input("Phone number: ").split(",")
-	information = name + address + phone_num
-	return information
+	def WriteFile(self):
+		with open(os.path.join(directory, filename+".txt"),"w") as text_file: # checks if directory exists; join and writes new file to the path of the first argument; .txt means its a text file
+			text_file.write(AskInformation()) # writes the information to the new file from passed function
 	
-def main():
-	directory = input("Which directory would you like your file to be saved? ")
-	filename = input("What file name do you want your files to be saved as? ")
-
-	try:
-		with open(os.path.join(directory, filename+".txt"),"w") as text_file:
-			json.dump(AskInformation(), text_file)
-
-	except FileNotFoundError:	
-		print("Directory does not exist! Please create a new directory: ")
-		new_directory = input("")
-		os.mkdir(new_directory)
-
-		with open(os.path.join(new_directory, filename+".txt"),"w") as text_file:
-					json.dump(AskInformation(), text_file)
-	else:		
-		print("Output Information:")
-
-		with open(os.path.join(directory, filename+".txt"),"r") as read_file:
+	def DisplayFile(self):
+		print("\nOutput Information:")
+		with open(os.path.join(self.directory, self.filename+".txt"),"r") as read_file: # opens and reads file with the path of the directory
 			contents = read_file.read()
 			print(contents)
 
+	def AskInformation(self):
+		"""Get input information from user"""
+		 # ask inputs from user and making sure its converted into strings
+		name = str(input("Name: "))
+		address = str(input("Address: "))
+		phone_num = str(input("Phone number: "))
+		return f"{name}, {address}, {phone_num}" # inputs will be formed in this output string when called
 
+def main():
+	""" Executes all for functionalitites needed for program. """
+	directory = input("Which directory would you like your file to be saved? ") # ask input for the path of the directory
+	filename = input("What file name do you want your files to be saved as? ") # ask input for the name of the text file
 
+	program = Files(directory, filename)
+
+	try: # 
+		program.WriteFile()
+		print(f"The Directory \"{directory}\" exists. Writing file \"{filename}\" in that directory....") # display output that directory exists
+
+	except FileNotFoundError: # catches error if directory was not found
+		print("Directory does not exist! Please create a new directory: ") # displays output that the directory was not found
+		directory= input("") # ask user for new name for a creation of new directory and rewrites directory assignment
+		os.mkdir(directory) # command creates new directory
+
+		program.WriteFile()
+		program.DisplayFile()
+
+	else:
+		program.DisplayFile()
 main()
